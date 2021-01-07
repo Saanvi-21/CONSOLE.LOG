ee4032fa0a36e580c9325fb1b46a20979de20dff
saanvi <71885805+Saanvi-21@users.noreply.github.com>
2021-01-07 14:55:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log ("This is my first console project.");
+
+            console.log("Saanvi");
+
+            console.log("Nazre");
+
+            console.log("L");
+
+            console.log("Saanvi Nazre L");
+
+            console.log ("15");
+
+            console.log ("5");
+
+            console.log ("50");
+
+            console.log ("2");
+
+            console.log("OK?");
+
+            console.log("♥ my project");
